09d5dd91dfa7fba1afef6a474fd2a044996d6c18
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-02-08 23:06:55 +0300

    Create route

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -6,4 +6,24 @@ class RoutesController < ApplicationController
   def index
     @routes = Route.all
   end
+  
+  def new
+    @route = Route.new
+  end
+  
+  def create
+    @route = Route.new(route_params)
+    
+    if @route.save
+      redirect_to routes_path, notice: "Route was created"
+    else
+      render :new      
+    end
+  end
+  
+  private
+  
+  def route_params
+    params.require(:route).permit(:name)
+  end
 end
