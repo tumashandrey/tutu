c2d8ba541eb7fe1f39553651e4920db987d2e060
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-02-23 01:10:44 +0300

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222220840) do
+ActiveRecord::Schema.define(version: 20160222221023) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "Title"
@@ -45,5 +45,10 @@ ActiveRecord::Schema.define(version: 20160222220840) do
   end
 
   add_index "trains", ["route_id"], name: "index_trains_on_route_id"
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
