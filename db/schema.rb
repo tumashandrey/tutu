1b68d513876eae4e4e687c9553bd25f02413d772
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-02-23 01:09:21 +0300

    Add ticket model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222215423) do
+ActiveRecord::Schema.define(version: 20160222220840) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "Title"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 20160222215423) do
 
   create_table "routes", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
