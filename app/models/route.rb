a0213925ffe9d10d780f7673db63ddfe1b062b8c
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-02-24 09:06:04 +0300

    Обычно, в моделе пишут сначала ассоциации, потом валидации

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,6 @@
 class Route < ActiveRecord::Base
-  validates :name, presence: true
-  
   has_and_belongs_to_many :railway_stations
   has_many :trains
+  
+  validates :name, presence: true  
 end
