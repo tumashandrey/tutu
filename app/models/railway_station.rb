724742df0bfaefeeb63c24b6c75aa1b09120fbab
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-02-27 15:51:09 +0300

    Добавить возможность указать порядковый номер станции в маршруте (поле для сортировки). Учесть, что одна и та же станция может входить в разные маршруты и иметь разный порядковый номер в разных маршрутах.

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,4 +1,8 @@
 class RailwayStation < ActiveRecord::Base  
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
+  
+  def set_position(route, position)
+    self.railway_stations_routes.where(route: route).station_pos = position    
+  end
 end
