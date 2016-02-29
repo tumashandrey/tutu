ac864f0a191ddec9d7b284cecc4acf27d94a0e3f
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-03-01 00:05:01 +0300

    Реализовать через STI следующие типы вагонов:

--- app/models/seat_wagon.rb
+++ app/models/seat_wagon.rb
@@ -1,3 +1,7 @@
 class SeatWagon < Wagon
   validates :seat_places, presence: true
+  
+  def self.model_name
+      Wagon.model_name
+  end  
 end
--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -1,13 +1,22 @@
 class Wagon < ActiveRecord::Base
   ECONOM   = "Плацкарт"
   BUSINESS = "Купе"  
-  TYPES    = [ECONOM, BUSINESS]
-  
+  SV       = "СВ"
+  SEAT     = "Сидячий"  
+
+  TYPES_HASH    = { ECONOM  => EconomWagon.to_s, 
+                    BUSINESS => CoupeWagon.to_s,
+                    SV => SvWagon.to_s,
+                    SEAT => SeatWagon.to_s
+                   }
+               
   belongs_to :train
   
-  validates :pos, uniqueness: { scope: :train_id }
+  validates :pos, uniqueness: { scope: :train_id }, if: :train_id
+  validates :train, presence: true
   
   after_validation :update_pos, if: :train_id_changed?
+  before_create :update_wagon_type
   
   scope :order_asc, -> { order('pos ASC') }
   scope :order_desc, -> { order('pos DESC') }
@@ -20,4 +29,8 @@ class Wagon < ActiveRecord::Base
     max_pos = self.train.wagons.maximum(:pos) || 0
     self.pos = max_pos + 1
   end
+  
+  def update_wagon_type
+    self.wagon_type = TYPES_HASH.key(self.type)
+  end  
 end
