f294ef3d7c2a06b4fc371d78413e69799a722440
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-03-10 19:52:42 +0300

    Поиск через руби перебором всех объектов это самое плохое решение...

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,20 +9,10 @@ class SearchesController < ApplicationController
     
     @routes = []
     
-    Route.all.each do |route|
-      start = nil
-      finish = nil
-      route.railway_stations_routes.order(:station_pos).each do |station_route|
-        index = station_route.station_pos
-        station_id = station_route.railway_station.id
-        start ||= station_route if @start_station.id == station_id  # первая
-        finish = station_route if @end_station.id == station_id
-      end
-
-      if start && finish and start.station_pos < finish.station_pos
-        @routes << route
-      end
-    end
+    start_routes = Route.joins(:railway_stations_routes).where(railway_stations_routes: {railway_station_id: @start_station})
+    end_routes = Route.joins(:railway_stations_routes).where(railway_stations_routes: {railway_station_id: @end_station})
+    
+    @routes = start_routes & end_routes
     
     render 'results'
   end  
