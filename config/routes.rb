64f41bc0434f6e9d76eaa3c27abbff5221ebd870
Vladimir Khramtsov <vkhramtsov@alfabank.ru>
2016-03-13 01:15:39 +0300

    Добавить Twitter Bootstrap (через gem) и стилизовать внешний вид приложения (на свой вкус).

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   
   get 'welcome/index'
   
-  root 'welcome#index'
+  root 'searches#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
